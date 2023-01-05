0ab69c835f83906b52a79ceff1c2d6044810aee2
kael_yorio <yoriokael@gmail.com>
2023-01-05 17:21:31 -0300

    creacion de componente cabanas,hoja de estilo css y clases de bootstrap

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Footer from './componentes/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import Cabanas from './componentes/cabanas/Cabanas';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Cabanas />
       <Footer />
-    </div>
+    </>
   );
 }
 
--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+
 const Footer = () => {
   return (
     
@@ -20,7 +21,9 @@ const Footer = () => {
         <h5>Complejo</h5>
         <ul className="nav flex-column">
           <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Inicio</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Servicios</a></li>
           <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Cabañas</a></li>
+          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Testimonios</a></li>
           <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Ubicación</a></li>
           <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Contacto</a></li>
         </ul>
@@ -35,7 +38,7 @@ const Footer = () => {
         <h5>Contacto</h5>
         <ul className="nav flex-column">
         <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-          <li className="ms-3"><a className="text-muted" href="https://instagram.com/campingquemquem?igshid=OGQ2MjdiOTE="><svg className="bi" width={24} height={24}><use xlinkHref="#instagram" /></svg></a></li>
+          <li className="ms-3"><a className="" href="https://instagram.com/campingquemquem?igshid=OGQ2MjdiOTE="><svg className="bi bi-instagram" width={24} height={24}><use xlinkHref="#instagram" /></svg></a></li>
           <li className="ms-3"><a className="text-muted" href="https://m.facebook.com/campingquemquem/photos/a.1009718072728462/1010691542631115/?type=3&source=48&__tn__=EH-R"><svg className="bi" width={24} height={24}><use xlinkHref="#facebook" /></svg></a></li>
       </ul>
         </ul>
